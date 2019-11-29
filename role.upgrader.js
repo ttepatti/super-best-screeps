90f4c9eef73a997b05892b7e78b8b1db5ded7e74
Tim Tepatti <tim@tepatti.com>
2019-11-29 04:30:09 -0500

    Worked on upgrader, made potential invader

--- role.upgrader.js
+++ role.upgrader.js
@@ -3,26 +3,50 @@ var roleUpgrader = {
     /** @param {Creep} creep **/
     run: function(creep) {
 
-        if(creep.memory.upgrading && creep.store[RESOURCE_ENERGY] == 0) {
-            creep.memory.upgrading = false;
-            creep.say('🔄 harvest');
-        }
-        if(!creep.memory.upgrading && creep.store.getFreeCapacity() == 0) {
-            creep.memory.upgrading = true;
-            creep.say('⚡ upgrade');
+        function goHome() {
+            var spawn = new RoomPosition(36, 31, 'W3N48');
+            // Check current coords, are we already at spawn?
+            if (creep.pos = spawn) {
+                creep.memory.goHome = false;
+            } else {
+                // move to my current spawner
+                creep.moveTo(spawn, {visualizePathStyle: {stroke: '#ffffff'}});
+            }
         }
 
-        if(creep.memory.upgrading) {
-            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
+        // above all else, go the fuck home
+        if(creep.memory.goHome) {
+            goHome();
+        } else {
+            // Stop upgrading if out of energy
+            if(creep.memory.upgrading && creep.store[RESOURCE_ENERGY] == 0) {
+                creep.memory.upgrading = false;
+                creep.say('🔄 harvest');
+            }
+            
+            // Upgrade if full of energy
+            if(!creep.memory.upgrading && creep.store.getFreeCapacity() == 0) {
+                creep.memory.upgrading = true;
+                creep.say('⚡ upgrade');
+            }
+
+            
+            if(creep.memory.upgrading) {
+                if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
+                } else if(creep.upgradeController(creep.room.controller) == ERR_NOT_OWNER) {
+                    creep.memory.goHome = true;
+                }
+            }
+            else {
+                var sources = creep.room.find(FIND_SOURCES);
+                if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+                }
             }
         }
-        else {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-            }
-        }
+        
+
     }
 };
 
